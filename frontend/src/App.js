5ab65a064b9724d7bad614c5fba790005ac2f376
Vu My <tranhoangvumy22@gmail.com>
2024-05-15 19:16:34 -0400

    Set Up axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import axiosClient from './axios-client';
+const payload = {
+  email: "tranmy0122english@gmail.com",
+  password: "admin1234",
+};
 function App() {
+  const handleSubmit = async () => {
+    await axiosClient.get('/sanctum/csrf-cookie');
+    await axiosClient.post("login", payload);
+  }
   return (
     <div className="App">
-
+      <button onClick={handleSubmit}>Fetch API </button>
     </div>
   );
 }
